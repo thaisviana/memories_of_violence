890a886c3e5eb6de8fda396201d0ea5e338fbb3c
thaisviana <thaisnviana+git@gmail.com>
2018-10-09 17:00:10 -0300

    static

--- memories/settings.py
+++ memories/settings.py
@@ -123,3 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+# Static server for django in production
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
